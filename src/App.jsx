66c9a6c2612ac1a813e49450831942284b8aa4d6
abhiram1208 <abhiramkrishna286@gmail.com>
2026-04-15 20:58:20 +0530

    frontend updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <>
-    <div>
-      <h1>Welcome to website demo</h1>
+    <div className='page'>
+      <h1 className='head'>Welcome to My application</h1>
+      <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum amet error beatae nobis sequi aut, est quod ex harum perferendis alias enim, nam nisi. Magni, fugiat voluptates. Harum, fugiat obcaecati.
+      Quibusdam officia officiis delectus veniam ad libero laudantium aliquam aliquid assumenda modi doloribus dolorem dignissimos dolores molestiae adipisci enim harum aut, soluta natus neque provident voluptate iste. Quia, voluptatum. Beatae?</p>
+      <div className='btns'>
+        <button>Explore More</button>
+        <button>Connect Us</button>
+      </div>
     </div>
-    </>
   )
 }
 
