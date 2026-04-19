c0568828f2cb78633b2dcc4a87eb97925ee3cd39
abhiram1208 <abhiramkrishna286@gmail.com>
2026-04-19 21:55:34 +0530

    index js updated

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send("Backend HOME page is RUNNING...")
 })
 
-app.listen(5000, () => {
-    console.log("Backend running on : http://localhost:5000")
+app.listen(5000,'0.0.0.0.', () => {
+    console.log("Backend running on :5000")
 })
